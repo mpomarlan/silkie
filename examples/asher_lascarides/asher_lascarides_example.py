19f79058ba698b90cda8170d9a600427e500f34d
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2022-07-29 11:08:17 +0200

    Asher-Lascarides elaboration example.

--- examples/asher_lascarides/asher_lascarides_example.py
+++ examples/asher_lascarides/asher_lascarides_example.py
@@ -19,14 +19,18 @@ def main():
     factsNarration = silkie.loadDFLFacts('./facts_example_narration.dfl', factsNarration)
     factsCausation = silkie.loadDFLFacts('./facts_base.dfl')
     factsCausation = silkie.loadDFLFacts('./facts_example_causation.dfl', factsCausation)
+    factsElaboration = silkie.loadDFLFacts('./facts_base.dfl')
+    factsElaboration = silkie.loadDFLFacts('./facts_example_elaboration.dfl', factsElaboration)
     
     theoryNarration, s2iNarration, i2sNarration, theoryStrNarration = silkie.buildTheory(rules,factsNarration,{},debugTheory=True)
     theoryCausation, s2iCausation, i2sCausation, theoryStrCausation = silkie.buildTheory(rules,factsCausation,{},debugTheory=True)
+    theoryElaboration, s2iElaboration, i2sElaboration, theoryStrElaboration = silkie.buildTheory(rules,factsElaboration,{},debugTheory=True)
     
-    for scnSpec in [(theoryNarration, i2sNarration, 'Narration: Max stood up. John greeted him'), (theoryCausation, i2sCausation, 'Causation: Max fell. John pushed him')]:
+    for scnSpec in [(theoryNarration, i2sNarration, 'Narration: Max stood up. John greeted him.'), (theoryCausation, i2sCausation, 'Causation: Max fell. John pushed him.'), (theoryElaboration, i2sElaboration, 'Elaboration: The council built the bridge. The architect drew up the plans.')]:
         theory, i2s, msg = scnSpec
         print('==========\n%s' % msg)
-        reportTargetConclusions(theory, i2s, ['narration', '-narration', 'explanation', '-explanation'])
+        reportTargetConclusions(theory, i2s, ['narration', '-narration', 'explanation', '-explanation', 'elaboration', '-elaboration'])
 
 if __name__ == '__main__':
     main()
+

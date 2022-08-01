d7eb24bc4f034960087c0fd4fc564599666ca975
Mihai Pomarlan <mpomarlan@yahoo.co.uk>
2022-08-01 17:31:58 +0200

    Some rule refactoring, added the incoherent example.

--- examples/asher_lascarides/asher_lascarides_example.py
+++ examples/asher_lascarides/asher_lascarides_example.py
@@ -25,14 +25,17 @@ def main():
     factsBackground = silkie.loadDFLFacts('./facts_example_background.dfl', factsBackground)
     factsResult = silkie.loadDFLFacts('./facts_base.dfl')
     factsResult = silkie.loadDFLFacts('./facts_example_result.dfl', factsResult)
+    factsIncoherent = silkie.loadDFLFacts('./facts_base.dfl')
+    factsIncoherent = silkie.loadDFLFacts('./facts_example_incoherent.dfl', factsIncoherent)
     
     theoryNarration, s2iNarration, i2sNarration, theoryStrNarration = silkie.buildTheory(rules,factsNarration,{},debugTheory=True)
     theoryCausation, s2iCausation, i2sCausation, theoryStrCausation = silkie.buildTheory(rules,factsCausation,{},debugTheory=True)
     theoryElaboration, s2iElaboration, i2sElaboration, theoryStrElaboration = silkie.buildTheory(rules,factsElaboration,{},debugTheory=True)
     theoryBackground, s2iBackground, i2sBackground, theoryStrBackground = silkie.buildTheory(rules,factsBackground,{},debugTheory=True)
     theoryResult, s2iResult, i2sResult, theoryStrResult = silkie.buildTheory(rules,factsResult,{},debugTheory=True)
+    theoryIncoherent, s2iIncoherent, i2sIncoherent, theoryStrIncoherent = silkie.buildTheory(rules,factsIncoherent,{},debugTheory=True)
     
-    for scnSpec in [(theoryNarration, i2sNarration, 'Narration: Max stood up. John greeted him.'), (theoryCausation, i2sCausation, 'Causation: Max fell. John pushed him.'), (theoryElaboration, i2sElaboration, 'Elaboration: The council built the bridge. The architect drew up the plans.'), (theoryBackground, i2sBackground, 'Background: Max opened the door. The room was dark.'), (theoryResult, i2sResult, 'Result: Max switched off the light. The room was dark.')]:
+    for scnSpec in [(theoryNarration, i2sNarration, 'Narration: Max stood up. John greeted him.'), (theoryCausation, i2sCausation, 'Causation: Max fell. John pushed him.'), (theoryElaboration, i2sElaboration, 'Elaboration: The council built the bridge. The architect drew up the plans.'), (theoryBackground, i2sBackground, 'Background: Max opened the door. The room was dark.'), (theoryResult, i2sResult, 'Result: Max switched off the light. The room was dark.'), (theoryIncoherent, i2sIncoherent, 'Incoherent: Max won the race. Max was home. (expecting no positive relation identified here.)')]:
         theory, i2s, msg = scnSpec
         print('==========\n%s' % msg)
         reportTargetConclusions(theory, i2s, ['narration', '-narration', 'explanation', '-explanation', 'elaboration', '-elaboration', 'background', '-background', 'result', '-result'])
